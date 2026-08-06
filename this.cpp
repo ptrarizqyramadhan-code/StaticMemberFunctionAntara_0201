0f8c33227b6ddb996b6a98042ab9bd76838b2573
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-06 21:55:23 +0700

    fisrt comit dari this

---
+++ this.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+
+public:
+    string setget(string a){
+        this->judul = a; //nilai parameter 'judul' untuk member variabel 'judul'
