1a294345343853efe4d049331c91a084a9ed83be
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-06 21:46:35 +0700

    deklarasi variabel member

--- staticmember.cpp
+++ staticmember.cpp
@@ -2,3 +2,9 @@
 using namespace std;
 
 class Mahasiswa { //membuat class mahasiswa
+
+public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
